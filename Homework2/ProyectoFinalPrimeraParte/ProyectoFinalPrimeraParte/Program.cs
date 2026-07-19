c20ebfb680b02161967c26bb96b3dfefae583db3
geylixlara <geylara08@gmail.com>
2026-07-19 09:54:03 -0400

    Agregando clase Habitacion

--- Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
+++ Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿public class Habitacion
+{
+    public int Numero { get; set; }
+    public string Tipo { get; set; }
+    public decimal Precio { get; set; }
+    public bool Disponible { get; set; }
+
+    public Habitacion(int numero, string tipo, decimal precio)
+    {
+        Numero = numero;
+        Tipo = tipo;
+        Precio = precio;
+        Disponible = true;  
+    }
+
+    public override string ToString()
+    {
+
+        return $"Habitacion {Numero} - {Tipo} - ${Precio} - {(Disponible ? "Disponible" : "Ocupada")}";
+    }
+}
