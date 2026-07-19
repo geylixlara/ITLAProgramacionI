5fbb325e2478b890ad1ee9102059add815a38b3e
geylixlara <geylara08@gmail.com>
2026-07-19 10:00:40 -0400

    Agregando seccion Huespedes

--- Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
+++ Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
@@ -19,3 +19,23 @@
         return $"Habitacion {Numero} - {Tipo} - ${Precio} - {(Disponible ? "Disponible" : "Ocupada")}";
     }
 }
+public class Huesped
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; }
+    public string Telefono { get; set; }
+    public string Correo { get; set; }
+
+    public Huesped(int id, string nombre, string telefono, string correo)
+    {
+        Id = id;
+        Nombre = nombre;
+        Telefono = telefono;
+        Correo = correo;
+    }
+
+    public override string ToString()
+    {
+        return $"ID: {Id} - {Nombre} - {Telefono} - {Correo}";
+    }
+}
