fb708d3adda756ed9a4c8c9195fb6eff7e362ad9
geylixlara <geylara08@gmail.com>
2026-07-19 10:11:50 -0400

    Agregando clase Reserva

--- Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
+++ Homework2/ProyectoFinalPrimeraParte/ProyectoFinalPrimeraParte/Program.cs
@@ -39,3 +39,28 @@ public class Huesped
         return $"ID: {Id} - {Nombre} - {Telefono} - {Correo}";
     }
 }
+public class Reserva
+{
+    public int Id { get; set; }
+    public Huesped Huesped { get; set; }
+    public Habitacion Habitacion { get; set; }
+    public DateTime FechaEntrada { get; set; }
+    public DateTime FechaSalida { get; set; }
+    public string Estado { get; set; }
+
+    public Reserva(int id, Huesped huesped, Habitacion habitacion, DateTime entrada, DateTime salida)
+    {
+        Id = id;
+        Huesped = huesped;
+        Habitacion = habitacion;
+        FechaEntrada = entrada;
+        FechaSalida = salida;
+        Estado = "Activa";
+        habitacion.Disponible = false;
+    }
+
+    public override string ToString()
+    {
+        return $"Reserva #{Id} - {Huesped.Nombre} - Hab.{Habitacion.Numero} - {Estado}";
+    }
+}
